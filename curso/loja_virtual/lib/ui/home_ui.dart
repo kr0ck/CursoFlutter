252c5b450f19ef3e736f4ea45096755c0344b0cf
hebert <hebertcampos@hotmail.com.br>
2019-04-19 19:47:23 -0300

    tela inicial criada

--- curso/loja_virtual/lib/ui/home_ui.dart
+++ curso/loja_virtual/lib/ui/home_ui.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final _pageCtrl = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return PageView(
+      controller: _pageCtrl,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.red,
+        )
+      ],
     );
   }
 }
