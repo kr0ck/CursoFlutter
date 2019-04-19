251d12bd4c83afb65a2a7706b814cd94a743ac8a
hebert <hebertcampos@hotmail.com.br>
2019-04-19 19:59:32 -0300

    criando o fundo com estilo gradiente

--- curso/loja_virtual/lib/tab/home_tab.dart
+++ curso/loja_virtual/lib/tab/home_tab.dart
@@ -3,8 +3,24 @@ import 'package:flutter/material.dart';
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+    Widget _buildBodyBack() => Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 211, 118, 130),
+            Color.fromARGB(255, 253, 181, 168)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+        )
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack()
+      ],
     );
   }
 }
