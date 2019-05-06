a14c2c5d6772884121cd02a027a4d8da3c6a3526
hebert <hebertcampos@hotmail.com.br>
2019-05-06 17:29:21 -0300

    teminando a listagem de categorias

--- curso/loja_virtual/lib/tab/products_tab.dart
+++ curso/loja_virtual/lib/tab/products_tab.dart
@@ -7,17 +7,19 @@ class ProductsTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder<QuerySnapshot>(
       future: Firestore.instance.collection('products').getDocuments(),
-      builder: (context, snapshot){
+      builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator(),);
-        } else {
-          return ListView(
-            children: snapshot.data.documents.map(
-              (doc){
-                return CategoryTile(doc);
-              }
-            ).toList(),
+          return Center(
+            child: CircularProgressIndicator(),
           );
+        } else {
+          var dividedTiles = ListTile.divideTiles(
+                  tiles: snapshot.data.documents.map((doc) {
+                    return CategoryTile(doc);
+                  }).toList(),
+                  color: Colors.grey[500])
+              .toList();
+          return ListView(children: dividedTiles);
         }
       },
     );
