d47b0d04e4a9ba9e08b4cfc3d1f1472781939af8
hebert <hebertcampos@hotmail.com.br>
2019-05-06 17:40:47 -0300

    criando tela de produtos da categoria e criando o ducumento

---
+++ curso/loja_virtual/lib/ui/category_ui.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+
+  final DocumentSnapshot snapshot;
+  
+  CategoryScreen(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
