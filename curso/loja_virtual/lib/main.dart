105f1f0d93e2d474ea4bee8e2e85f7b4f6472edc
hebert <hebertcampos@hotmail.com.br>
2019-05-23 16:44:32 -0300

    Criando novo usuario

--- curso/loja_virtual/lib/main.dart
+++ curso/loja_virtual/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/ui/home_ui.dart';
+import 'package:scoped_model/scoped_model.dart';
 //import 'package:loja_virtual/ui/login_ui.dart';
 
 void main() => runApp(MyApp());
@@ -7,14 +9,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter`s Clothing',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141)
+    return ScopedModel<UserModel>(
+      model: UserModel(),
+      child: MaterialApp(
+        title: 'Flutter`s Clothing',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: Color.fromARGB(255, 4, 125, 141)),
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(), //HomeScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),//HomeScreen(),
     );
   }
 }
--- curso/loja_virtual/lib/ui/signup_ui.dart
+++ curso/loja_virtual/lib/ui/signup_ui.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class SingUpScreen extends StatelessWidget {
+class SingUpScreen extends StatefulWidget {
+  @override
+  _SingUpScreenState createState() => _SingUpScreenState();
+}
+
+class _SingUpScreenState extends State<SingUpScreen> {
+  final _nameCtrl = TextEditingController();
+  final _passCtrl = TextEditingController();
+  final _emailCtrl = TextEditingController();
+  final _addressCtrl = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -9,74 +20,101 @@ class SingUpScreen extends StatelessWidget {
         title: Text('Criar conta'),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Nome Completo'),
-              validator: (text) {
-                if (text.isEmpty || text.length < 3) {
-                  return 'Nome inválido!!!';
-                }
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Endereço'),
-              validator: (text) {
-                if (text.isEmpty || text.length < 3) {
-                  return 'Endereço inválido!!!';
-                }
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'E-mail'),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || !text.contains('@')) {
-                  return 'E-mail inválido!!!';
-                }
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: 'Senha'),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) {
-                  return 'Senha inválida!!!';
-                }
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 44.0,
-              child: RaisedButton(
-                child: Text(
-                  'Criar Conta',
-                  style: TextStyle(fontSize: 18.0),
-                ),
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
+      body: ScopedModelDescendant<UserModel>(builder: (context, child, model) {
+        if (model.isLoading) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.all(16.0),
+            children: <Widget>[
+              TextFormField(
+                controller: _nameCtrl,
+                decoration: InputDecoration(hintText: 'Nome Completo'),
+                validator: (text) {
+                  if (text.isEmpty || text.length < 3) {
+                    return 'Nome inválido!!!';
+                  }
                 },
               ),
-            ),
-          ],
-        ),
-      ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                controller: _addressCtrl,
+                decoration: InputDecoration(hintText: 'Endereço'),
+                validator: (text) {
+                  if (text.isEmpty || text.length < 3) {
+                    return 'Endereço inválido!!!';
+                  }
+                },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                controller: _emailCtrl,
+                decoration: InputDecoration(hintText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                validator: (text) {
+                  if (text.isEmpty || !text.contains('@')) {
+                    return 'E-mail inválido!!!';
+                  }
+                },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                controller: _passCtrl,
+                decoration: InputDecoration(hintText: 'Senha'),
+                obscureText: true,
+                validator: (text) {
+                  if (text.isEmpty || text.length < 6) {
+                    return 'Senha inválida!!!';
+                  }
+                },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              SizedBox(
+                height: 44.0,
+                child: RaisedButton(
+                  child: Text(
+                    'Criar Conta',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  textColor: Colors.white,
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      Map<String, dynamic> userData = {
+                        'name': _nameCtrl.text,
+                        'email': _emailCtrl.text,
+                        'address': _addressCtrl.text
+                      };
+
+                      model.singUp(
+                        onfail: _onFail,
+                        onSuccess: _onSucess,
+                        pass: _passCtrl.text,
+                        userData: userData,
+                      );
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
+
+  void _onSucess() {}
+  void _onFail() {}
 }
